3311b83b973ddc937a6ad9309d69fe17bf92609b
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-16 15:07:31 +0200

    Feat(auth): implement authentication with credentials folder, add loginWithServiceAccount for delegated credentials

--- auth.py
+++ auth.py
@@ -4,6 +4,9 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+from constants import defaultEmail
 
 SCOPES = [
     "https://www.googleapis.com/auth/admin.directory.user",
@@ -13,25 +16,34 @@ SCOPES = [
 
 creds = None
 
-def loginAndInitializeService():
+def loginWithOAuthToken(serviceName, serviceVersion):
     global creds
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-        print("| Logging in using saved credentials...")
+    if os.path.exists("credentials/token.json"):
+        creds = Credentials.from_authorized_user_file("credentials/token.json", SCOPES)
+        print("| Logging in for "+serviceName+" using saved credentials...")
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            print("| Refreshing your credentials...")
+            print("| Refreshing your credentials")
             creds.refresh(Request())
         else:
-            print("| Use your browser to login...")
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            print("| Use your browser to login")
+            flow = InstalledAppFlow.from_client_secrets_file("credentials/oAuth_credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open("credentials/token.json", "w") as token:
             token.write(creds.to_json())
 
     # Initialize an admin service with given credentials
-    print("| Authentication succeeded!")
-    return build("admin", "directory_v1", credentials=creds)
+    print("| Authentication for "+serviceName+" succeeded!")
+    return build(serviceName, serviceVersion, credentials=creds)
+
+
+def loginWithDomainLevelServiceAccount(serviceName, serviceVersion, email=defaultEmail):
+    print("| Logging in for "+serviceName+" using service account credentials...")
+    credentials = service_account.Credentials.from_service_account_file(
+        "credentials/serviceAccount_credentials.json", scopes=SCOPES)
+    delegated_credentials = credentials.with_subject(email) 
+    print("| Creating delegated credentials for "+email+"\n|")
+    return build(serviceName, serviceVersion, credentials=delegated_credentials)
