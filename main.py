ae0377a266b6cc8e76beb67d3c5bf64db6fc5679
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-16 15:12:30 +0200

    Feat(New Auth and Calendars): add new auth structure and calendar list

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time, os 
-from auth import loginAndInitializeService
-from users import getAllUsers, printFormattedUsers
-from groups import getAllGroups, printFormattedGroups
-from members import getMembersFromGroupName
 from constants import maxRowLength
 from utils import printBoxAndAskUser, printTitle, printClosure
 
-service = None
+from auth import loginWithOAuthToken, loginWithDomainLevelServiceAccount
+
+from users import getAllUsers, printFormattedUsers
+from groups import getAllGroups, printFormattedGroups
+from members import getMembersFromGroupName
+from calendars import getCalendarListFromUserEmail, printCalendarList
+
+directoryService = None
 
 def welcome_script():
-    global service
+    global directoryService
     os.system('clear')
     print("\n" + "-"*maxRowLength)
     print("| Welcome to the Google Workspace Utils script!")
@@ -17,9 +20,9 @@ def welcome_script():
     time.sleep(0.5)
 
     print("\n" + "-"*3 + "Auth" + "-"*(maxRowLength-4-3))
-    print("| Initializing authentication process...")
+    print("| Initializing authentication process...\n|")
     time.sleep(1)
-    service = loginAndInitializeService()
+    directoryService = loginWithOAuthToken("admin", "directory_v1")
     print("-"*maxRowLength + "\n\n")
 
     while True: 
@@ -27,6 +30,7 @@ def welcome_script():
         print("| [1] Get All Users")
         print("| [2] Get All Groups")
         print("| [3] Get All Members of a Group")
+        print("| [4] Get All Calendars of a User")
         print("| [q] Quit")
 
         
@@ -35,20 +39,25 @@ def welcome_script():
         
         if choice == "1":
             printTitle("Users")
-            printFormattedUsers(getAllUsers(service))
+            printFormattedUsers(getAllUsers(directoryService))
             printClosure()
             time.sleep(2)
         elif choice == "2":
             printTitle("Groups")
-            printFormattedGroups(getAllGroups(service))
+            printFormattedGroups(getAllGroups(directoryService))
             printClosure()
             time.sleep(2)
         elif choice == "3":
             name = printBoxAndAskUser(title="Group Name", label="Enter group name")
             printTitle("Members of "+name)
-            printFormattedUsers(getMembersFromGroupName(service, name))
+            printFormattedUsers(getMembersFromGroupName(directoryService, name))
             printClosure()
             time.sleep(2)
+        elif choice == "4":
+            email = printBoxAndAskUser(title="User Email", label="Enter user email")
+            printTitle("Calendars of "+email)
+            printCalendarList(getCalendarListFromUserEmail(email))
+            printClosure()
         elif choice == "q":
             print("\n" + "-"*maxRowLength)
             print("| Quitting the program, goodbye! ")
