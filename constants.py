c27019d00505a76f30d7cb462acc03976cb09f68
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-16 02:52:12 +0200

    Feat(Constants): implement constants for max row length

---
+++ constants.py
@@ -0,0 +1,1 @@
+maxRowLength = 120
--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import time, os 
 from auth import loginAndInitializeService
+from users import getAndPrintAllUsers
+from constants import maxRowLength
 
 service = None
 
 def welcome_script():
     global service
     os.system('clear')
-    print("\n" + "-"*75)
+    print("\n" + "-"*maxRowLength)
     print("| Welcome to the Google Workspace Utils script!")
-    print("-"*75 + "\n\n")
+    print("-"*maxRowLength + "\n\n")
     time.sleep(0.5)
 
-    print("\n" + "-"*3 + "Auth" + "-"*(75-4))
+    print("\n" + "-"*3 + "Auth" + "-"*(maxRowLength-4-3))
     print("| Initializing authentication process...")
     time.sleep(1)
     service = loginAndInitializeService()
-    print("-"*75 + "\n\n")
+    print("-"*maxRowLength + "\n\n")
 
     while True: 
-        print("\n" + "-"*3 + "Supported functions" + "-"*(72-19))
+        print("\n" + "-"*3 + "Supported functions" + "-"*(maxRowLength-19-3))
         print("| [1] Get All Users")
         print("| [2] Get All Groups")
         print("| [3] Get All Members of a Group")
+        print("| [q] Quit")
+
         
         choice = input("|\n| Enter your choice: ")
-        print("-"*75 + "\n")
+        print("-"*maxRowLength + "\n")
         
         if choice == "1":
-            print("1")
+            getAndPrintAllUsers(service)
             time.sleep(2)
         elif choice == "2":
             print("2")
@@ -36,9 +40,10 @@ def welcome_script():
             print("3")
             time.sleep(2)
         elif choice == "q":
-            print("\n" + "-"*75)
+            print("\n" + "-"*maxRowLength)
             print("| Quitting the program, goodbye! ")
-            print("-"*75 + "\n")
+            print("-"*maxRowLength + "\n")
+            break
         else:
             print("Invalid choice. Please try again.")
             time.sleep(0.5)
