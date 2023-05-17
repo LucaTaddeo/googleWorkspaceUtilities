49ae17936131aaf2bd5229f8889e886942ce8d3e
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-17 18:03:24 +0200

    Feat(ACL Rule): implement creation of ACL Rules for calendar

--- calendars.py
+++ calendars.py
@@ -1,14 +1,11 @@
 from auth import loginWithDomainLevelServiceAccount
 from utils import getColoredText
 
-def getCalendarListFromUserEmail(userEmail):
-    delegatedService = loginWithDomainLevelServiceAccount("calendar", "v3", userEmail)
-    if delegatedService is not None:
-        try:
-            return delegatedService.calendarList().list().execute().get("items")
-        except:
-            print("| "+getColoredText("Unable to get calendars for "+getColoredText(userEmail, "bold"), "red"))
-    else: return None
+def getCalendarListFromUserEmail(service, userEmail):
+    try:
+        return service.calendarList().list().execute().get("items")
+    except:
+        print("| "+getColoredText("Unable to get calendars for "+getColoredText(userEmail, "bold"), "red"))
 
 def printCalendarList(calendars):
     if not calendars:
@@ -18,14 +15,28 @@ def printCalendarList(calendars):
             printSingleCalendar(calendar)
 
 def printSingleCalendar(calendar): 
-     print(u'| [ID: {0}] {1}'.format(calendar['id'], calendar['summary']))
+    print(u'| [ID: {0}] {1}'.format(calendar['id'], calendar['summary']))
 
-def addCalendarToUser(userEmail, calendarId):
-    delegatedService = loginWithDomainLevelServiceAccount("calendar", "v3", userEmail)
-    if delegatedService is not None:
-        try:
-            return delegatedService.calendarList().insert(body={"id": calendarId}).execute()
-        except:
-            print("| "+getColoredText("Error: Impossible to add the calendar to the calendarList!", "red"))
-    else: return None
-         
+def addCalendarToUser(service, calendarId):
+    try:
+        return service.calendarList().insert(body={"id": calendarId}).execute()
+    except:
+        print("| "+getColoredText("Error: Impossible to add the calendar to the calendarList!", "red"))
+
+def createAccessControlRule(service, userEmail, calendarId, role="reader", scope="user"):
+    print("| Creating ACL Rule...")
+    try:
+        rule = {
+            'scope': {
+                'type': scope,
+                'value': userEmail
+            },
+            'role': role
+        }
+
+        res = service.acl().insert(calendarId=calendarId, body=rule).execute()
+        print("| " + getColoredText(u'ACL Rule for {0} {1} with role {2} created'
+                                    .format(res['scope']['type'], res['scope']['value'], res['role']), "green"))
+        return res
+    except: 
+        print("| "+getColoredText("Error: unable to create ACL rule!", "red"))
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import time, os 
-from constants import maxRowLength
-from utils import printBoxAndAskUser, printTitle, printClosure
+from constants import maxRowLength, aclRoles
+from utils import printBoxAndAskUser, printTitle, printClosure, printBoxAndAskUserWithOptions
 
-from auth import loginWithOAuthToken
+from auth import loginWithOAuthToken, loginWithDomainLevelServiceAccount
 
 from users import getAllUsers, printFormattedUsers
 from groups import getAllGroups, printFormattedGroups
 from members import getMembersFromGroupName
-from calendars import getCalendarListFromUserEmail, printCalendarList, addCalendarToUser
+from calendars import getCalendarListFromUserEmail, printCalendarList, addCalendarToUser, createAccessControlRule
 
 directoryService = None
+calendarService = None
+
+def getDelegatedService(email):
+    printTitle("Delegated Auth: "+email)
+    delegatedService = loginWithDomainLevelServiceAccount("calendar", "v3", email)
+    printClosure()
+    return delegatedService
 
 def welcome_script():
     global directoryService
@@ -22,7 +29,11 @@ def welcome_script():
     printTitle("Auth")
     print("| Initializing authentication process...\n|")
     time.sleep(1)
+
     directoryService = loginWithOAuthToken("admin", "directory_v1")
+    print("|")
+    calendarService = loginWithOAuthToken("calendar", "v3")
+    
     print("-"*maxRowLength + "\n\n")
 
     while True: 
@@ -32,8 +43,8 @@ def welcome_script():
         print("| [3] List Members of Group")
         print("| [4] List Calendars of User")
         print("| [5] Add Calendar to User")
+        print("| [6] Create ACL Rule")
         print("| [q] Quit")
-
         
         choice = input("|\n| Enter your choice: ")
         print("-"*maxRowLength + "\n")
@@ -56,18 +67,33 @@ def welcome_script():
             time.sleep(2)
         elif choice == "4":
             email = printBoxAndAskUser(title="User Email", label="Enter user email")
+            delegatedService = getDelegatedService(email)
             printTitle("Calendars of "+email)
-            printCalendarList(getCalendarListFromUserEmail(email))
+            printCalendarList(getCalendarListFromUserEmail(delegatedService, email))
             printClosure()
         elif choice == "5":
             email = printBoxAndAskUser(title="User Email", label="Enter user email")
+            delegatedService = getDelegatedService(email)
             calendarId = printBoxAndAskUser(title="Calendar Id", label="Enter calendar id")
             #ask for background color  
             printTitle("Add "+calendarId+" to "+email)
             res = addCalendarToUser(email, calendarId)
             print(u'| {0}'.format(res)) if res is not None else None
             print("| ")
+            print("| Calendar list of "+email+" after the addition: ")
             printCalendarList(getCalendarListFromUserEmail(email))
+            printClosure()
+        elif choice == "6":
+            email = printBoxAndAskUser(title="User Email", label="Enter email of user")
+            calendarId = printBoxAndAskUser(title="Calendar Id", label="Enter calendar id")
+            role = aclRoles.get(printBoxAndAskUserWithOptions(
+                title="Role", 
+                label="Enter role [ r | w | o | f ]", 
+                options=aclRoles.keys(), 
+                text="Available roles are: [r]eader, [w]riter, [o]wner or [f]ree and busy reader"
+            ))
+            printTitle("Create ACL rule for "+email)
+            res = createAccessControlRule(calendarService, email, calendarId, role)
             printClosure()
         elif choice == "q":
             print("\n" + "-"*maxRowLength)
