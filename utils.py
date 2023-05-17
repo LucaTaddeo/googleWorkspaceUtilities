8f27a6b988d44d9e0d07950d8c719d4a04d28e9a
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-17 16:02:08 +0200

    Feat(Colored Text Util): implement function to wrap text in ANSI colors

--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
-from constants import maxRowLength
+from constants import maxRowLength, ansiColors
+
+def getColoredText(text, colors):
+    if isinstance(colors, str):
+        colors = [colors] 
+
+    wrappedText = text
+    for color in colors:
+        if color in ansiColors:
+            color_code = ansiColors[color]
+            wrappedText = color_code + wrappedText + ansiColors["reset"]
+
+    return wrappedText
 
 def printBoxAndAskUser(title, label):
     printTitle(title)
@@ -7,7 +19,7 @@ def printBoxAndAskUser(title, label):
     return choice
 
 def printTitle(title):
-    print("\n" + "-"*3 + title + "-"*(maxRowLength-len(title)-3))
+    print("\n" + "-"*3 + getColoredText(title, "bold") + "-"*(maxRowLength-len(title)-3))
 
 def printClosure():
     print("-"*maxRowLength + "\n")
