36d02c6bf44f0e2c80b71d84488650068c802136
Luca Taddeo <lucataddeo2000@gmail.com>
2023-05-18 10:13:09 +0200

    Feat(Members by groupMail): implement function to get members of group by groupMail

--- members.py
+++ members.py
@@ -1,11 +1,11 @@
-from groups import getGroupByName
+from groups import getGroupByEmail
 
 def getMembersOfGroup(service, groupId):
     return service.members().list(groupKey=groupId).execute().get('members', [])
 
 
-def getMembersFromGroupName(service, name):
-    group = getGroupByName(service, name)
+def getMembersFromGroupEmail(service, email):
+    group = getGroupByEmail(service, email)
     if group is not None:
         return getMembersOfGroup(service, group['id'])
     else:
